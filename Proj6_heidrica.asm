5752c7d28f03511b87cac75ad0b898ef2fc0fb82
Adam Heidrick <adamheidrick@gmail.com>
2021-11-23 12:21:09 +0100

    Finished ReadVal for now. Moving on WriteVal

--- Proj6_heidrica.asm
+++ Proj6_heidrica.asm
@@ -41,110 +41,118 @@ max_input		DWORD		sizeof user_input
 byte_count		dword		?
 
 ; This is for the ReadVal Procedure
-conv_num		SDWORD		0										; holds the value of the converted number
-conv_accum		byte		31 DUP(0)								; array for accumulating conversion
+stored_num		SDWORD		?										; stores the value after conversion								
+conv_num		SDWORD		0										; holds the value while converting
 val_error		DWORD		0										; this is for indicating an error within ReadVal valuation
 
 
 .code
 main PROC
-	
-push	val_error			; +32
-push	offset conv_accum	; +28
+push	offset stored_num	; +32
+push	offset val_error	; +28
 push	conv_num			; +24
 push	offset user_prompt	; +20
 push	offset user_input	; +16
 push	max_input			; +12
 push	byte_count			; +8
-call ReadVal
+call	ReadVal
+call	CrLf
 
-
+mov		eax, stored_num
+call	writeInt
+mov		eax, val_error
+call	writeInt
 
 	Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
 ReadVal PROC
-; 20 user prompt. 16 user_input array. 12 size of input. 8 num of characters in array
-push		ebp
-mov			ebp, esp
-pushad
+	push		ebp
+	mov			ebp, esp
+	pushad
 
-mGetString [ebp+20], [ebp+16], [ebp+12], [ebp+8] ; MACRO TO GET USER INPUT
+	mGetString [ebp+20], [ebp+16], [ebp+12], [ebp+8] ; MACRO TO GET USER INPUT
 
-mov			ecx, [ebp+8]
-mov			esi, [ebp+16]
-mov			ecx, [ebp+8]						; Prep for loop
+
+	mov			ecx, [ebp+8]
+	mov			esi, [ebp+16]
+	mov			edi, [ebp+32]
+	mov			ecx, [ebp+8]						; Prep for loop
 
 ; These are preliminary checks: it checks if user just hinted enter with no value or too many characters. 
 
-mov			eax, [ebp+8]
-cmp			eax, 0
-jz			_error								; if does not enter value and just hits enter
-cmp			eax, 15
-ja			_error								; if user enters more than 10 characters, this is just a pre check. An overflow check is also in place in the conversion loop
+	mov			eax, [ebp+8]
+	cmp			eax, 0
+	jz			_error								; if does not enter value and just hits enter
+	cmp			eax, 15
+	ja			_error								; if user enters more than 10 characters, this is just a pre check. An overflow check is also in place in the conversion loop
 
 ; These next two checks check for the first value of the array for sign. 
 
-mov			al, [esi]
-cmp			al, 43
-je			_positive
-cmp			al, 45
-je			_negative
-jmp			_convertloop
+	mov			al, [esi]
+	cmp			al, 43
+	je			_positive
+	cmp			al, 45
+	je			_negative
+	jmp			_convertloop
 
 _positive:
-inc			esi
-dec			ecx
-jmp			_convertloop
+	inc			esi
+	dec			ecx
+	jmp			_convertloop
 
 _negative:
-inc			esi
-dec			ecx
-mov			edx, 1				; register used to determine if the value at the end needs to be negative
+	inc			esi
+	dec			ecx
+	mov			edx, 1				; register used to determine if the value at the end needs to be negative
 
 _convertloop:
 
-LODSB		; takes whatever value is in ESI and copies it to AL REG then ESI is pointed to the next item. 
-
-cmp			al, 48
-jl			_error
-cmp			al, 57
-jg			_error 
+	LODSB		; takes whatever value is in ESI and copies it to AL REG then ESI is pointed to the next item. 
+	
+	cmp			al, 48
+	jl			_error
+	cmp			al, 57
+	jg			_error 
 
 ; conversion starts here
-sub			al, 48
-mov			ebx, [ebp+24]
-push		eax
-mov			eax, 10d
-push		edx						; preserves the register that I am using to determine if the value needs to be negative
-mul			ebx
-pop			edx
-mov			ebx, eax
-pop			eax
-add			ebx, eax
-JO			_error					; -2147483648 -> 2147483647 for SDWORD. IF this overflows, then error is raised. 
+	sub			al, 48
+	mov			ebx, [ebp+24]
+	push		eax
+	mov			eax, 10d
+	push		edx						; preserves the register that I am using to determine if the value needs to be negative
+	mul			ebx
+	pop			edx
+	mov			ebx, eax
+	pop			eax
+	add			ebx, eax
+	JO			_error					; -2147483648 -> 2147483647 for SDWORD. IF this overflows, then error is raised. 
 
 ; accumulates in conv_accum ebp+24
-mov			[ebp+24], ebx
-loop		_convertloop
+	mov			[ebp+24], ebx
+	loop		_convertloop
 
-cmp			edx, 1
-jne			_done
-mov			eax, [ebp+24]
-neg			eax
-JO			_error
-mov			[ebp+24], eax
-jmp			_done
-
+; checks if number needs to be negative based on edx register set in _negative
+	cmp			edx, 1
+	jne			_done
+	mov			eax, [ebp+24]
+	neg			eax
+	JO			_error
+	mov			[ebp+24], eax
 
 _done:
-
-mov			eax, [ebp+24]
-call		writeInt
+	mov			eax, [ebp+24]
+	mov			[edi], eax
+	JO			_error
+	jmp			_exit
 
 _error:
-mov			eax, 1
-mov			[ebp+32], eax
+	mov			edi, [ebp+28]
+	mov			eax, 1
+	mov			[edi], eax
+
+_exit:
+
 
 popad
 pop			ebp
